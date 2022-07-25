39c5839789249e1e5960cbf17b3167291fae9451
lingj1206 <lingj1206@masseyhigh.school.nz>
2022-07-25 13:26:04 +1200

    Update ZZ_modulo_play.py

--- ZZ_modulo_play.py
+++ ZZ_modulo_play.py
@@ -12,4 +12,10 @@ while keep_going == "":
         lolly_pl = "lollies"
 
     print()
+    print("You have {} lollies and {} students".format(num_lollies, num_students))
+    print("Each student gets {} lollies".format(lollies_per_student))
+    print("You have {} left".format(lollies_left))
+    print()
     
+    keep_going = input ("Again <enter>")
+    print()
